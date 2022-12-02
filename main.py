d67488fdd3d6819a1a1924df89732f491e41d2df
N.V.Chudinovskikh@urfu.me <N.V.Chudinovskikh@urfu.me>
2022-12-02 09:48:10 +0500

    хачу чипсы

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import job, statistic
 
-job_or_statistic = input('Введите данные для печати: ')
+job_or_statistic = input('Введите что то для печати: ')
 if job_or_statistic == 'Вакансии':
     job.print_job_table()
 elif job_or_statistic == 'Статистика':
